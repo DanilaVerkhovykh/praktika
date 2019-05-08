cdc01894d2269e28c1d6b344b289cc7319617eef
DanilaVerkhovykh <49486146+DanilaVerkhovykh@users.noreply.github.com>
2019-05-08 16:00:40 +0300

    Add files via upload

---
+++ пр3/3.1.cpp
@@ -0,0 +1,31 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Book {
+	int pages;
+	string chapter;
+	string author;
+	string name_of_book;
+	int edition;
+
+	void booker() {
+		cout << "Страниц: " << pages << endl;
+		cout << "Глава: " << chapter << endl;
+		cout << "Автор: " << author << endl;
+		cout << "Название книги: " << name_of_book << endl;
+		cout << "Издание: " << edition << endl;
+	}
+};
+
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	Book my_book = { 420, "Вторая" , "Стивен Кинг", "Зелёная миля", 1996 };
+	my_book.booker();
+	system("pause");
+	return 0;
+}
