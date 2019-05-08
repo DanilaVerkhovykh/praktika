3d187778651f13c138c21c7c710e77983eb42716
DanilaVerkhovykh <49486146+DanilaVerkhovykh@users.noreply.github.com>
2019-05-08 15:15:22 +0300

    Add files via upload

---
+++ пр2/4.cpp
@@ -0,0 +1,33 @@
+﻿
+#include "pch.h"
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	float y, t;
+	float a = 3.5;
+	float x = -0.7;
+	if (a <= x) {
+		y = a + log(x + a);
+	}
+	if (a > x) {
+		y = sqrt(sin(a*x));
+	}
+	if (a > y) {
+		t = y / (a - x);
+	}
+	if (a == y) {
+		t = y / (a - x) + (a + x) / (y*y);
+	}
+	if (a < y) {
+		t = tan(a*x) + cos(2 * a*y);
+	}
+	cout << "Число a равно: " << a << endl;
+	cout << "Число x равно: " << x << endl;
+	cout << "Число y равно: " << y << endl;
+	cout << "Число t равно: " << t << endl;
+}
