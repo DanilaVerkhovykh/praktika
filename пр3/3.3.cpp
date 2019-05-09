f729668c7465648686362357158836f0b575dd91
DanilaVerkhovykh <49486146+DanilaVerkhovykh@users.noreply.github.com>
2019-05-09 16:02:25 +0300

    Add files via upload

---
+++ пр3/3.3.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct avto {
+	string marka;
+	string model;
+	int losh_sil;
+	int kolichestvo_dverey;
+	string privod;
+
+	void casco() {
+		cout << "Марка: " << marka << endl;
+		cout << "Модель: " << model << endl;
+		cout << "ЛС: " << losh_sil << endl;
+		cout << "Кол-во дверей: " << kolichestvo_dverey << endl;
+		cout << "Вид привода: " << privod << endl;
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	avto moe = { "Феррари", "488", 670, 2, "Задний" };
+	moe.casco();
+	system("pause");
+	return 0;
+}
