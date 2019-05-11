ae4b51ca912211c028e45c04cbf0253177793ee5
DanilaVerkhovykh <49486146+DanilaVerkhovykh@users.noreply.github.com>
2019-05-11 21:08:42 +0300

    Add files via upload

---
+++ пр1/1.2.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	float x, y, a, b;
+	cout << "Введите первое число:" << endl;
+	cin >> x;
+	cout << "Введите второе число:" << endl;
+	cin >> y;
+	a = x + y;
+	b = x - y;
+	cout << "Сумма чисел равна:" << a << endl;
+	cout << "Разность чисел равна:" << b << endl;
+}
