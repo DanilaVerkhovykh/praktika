ab538e9c2b9a56499711ee69ba86e8b1c10b8131
DanilaVerkhovykh <49486146+DanilaVerkhovykh@users.noreply.github.com>
2019-05-11 19:26:49 +0300

    Add files via upload

---
+++ пр1/1.1.cpp
@@ -0,0 +1,13 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char a[100];
+	setlocale(LC_ALL, "RUS");
+	cout << "Введите свое имя на английском языке:" << endl;
+	cin >> a;
+	cout << "Добрый день, " << a << endl;
+
+}
