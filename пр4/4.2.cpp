afeef76cefd07a7bda221c6ec0a3ecc9a6dc4639
DanilaVerkhovykh <49486146+DanilaVerkhovykh@users.noreply.github.com>
2019-05-11 23:57:28 +0300

    Add files via upload

---
+++ пр4/4.2.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int *change(int *a) {
+	for (int i = 0; i < 11; i = i + 2) {
+		int p = a[i];
+		a[i] = a[i + 1];
+		a[i + 1] = p;
+	}
+	return a;
+}
+
+void vyvod(int *a) {
+	for (int i = 0; i < 12; i++) {
+		cout << a[i] << " ";
+	}
+	cout << endl;
+}
+
+int main()
+{
+	srand(time(0));
+	int *a = new int[12];
+	for (int i = 0; i <= 11; i++) {
+		a[i] = rand() % 10 + 1;
+	}
+	vyvod(a);
+	a = change(a);
+	vyvod(a);
+}
