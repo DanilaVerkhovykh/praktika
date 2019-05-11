945a86762182045c993b30bd4fb5ae0926eb67f0
DanilaVerkhovykh <49486146+DanilaVerkhovykh@users.noreply.github.com>
2019-05-11 22:34:45 +0300

    Add files via upload

---
+++ пр3/3.4.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct tochka {
+	double x, y;
+};
+
+struct Vektor {
+	tochka a, b;
+};
+
+int main()
+{
+	Vektor ab = { {1,5}, {5.6,3.2} };
+	system("pause");
+	return 0;
+}
