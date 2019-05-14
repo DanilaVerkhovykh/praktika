1d582249f7e7a76807000ecd68c42c9ca6960a7e
DanilaVerkhovykh <49486146+DanilaVerkhovykh@users.noreply.github.com>
2019-05-14 22:11:55 +0300

    Add files via upload

---
+++ пр8/8.1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
+using namespace std;
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	ifstream fin("bin.txt");	// Открытие файла на считывание данных из него
+	while (!fin.eof()) {
+		string s;
+		fin >> s;	// Считывание данных из файла
+		cout << s << endl;
+	}
+	fin.close();	// Закрытие файла
+	system("pause");
+	return 0;
+}
